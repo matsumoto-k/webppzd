3029e3fc2002b7678aed380b637c4f77c4b249ea
matsumoto-k <ww.kei10@gmail.com>
2019-10-03 21:06:27 +0900

    test

---
+++ WebApplicationType4/WebApplicationType4/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebApplicationType4.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebApplicationType4/WebApplicationType4/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApplicationType4.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
